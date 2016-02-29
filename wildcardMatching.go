819b3c13ab0c1a316f0931dbcf0501f708525236
dogetime <gosin.gu@gmail.com>
2016-02-29 21:22:43 +0800

    Added files via upload

---
+++ wildcardMatching.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isMatch(inputStr string, matchStr string) bool {
+	inputStart := 0
+	matchStart := 0
+
+	for inputStart != len(inputStr) {
+		if matchStr[matchStart:matchStart+1] == "*" {
+			matchStart++
+			if matchStart == len(matchStr) {
+				return true
+			}
+
+		} else if matchStr[matchStart:matchStart+1] == "?" ||
+			inputStr[inputStart:inputStart+1] == matchStr[matchStart:matchStart+1] {
+			matchStart++
+			inputStart++
+			if matchStart == len(matchStr) {
+				return true
+
+			}
+		} else if inputStart != 0 {
+			inputStart++
+
+		}
+
+	}
+
+	return false
+
+}
+
+func main() {
+
+	fmt.Println(isMatch("hello", "h?llo"))
+}
