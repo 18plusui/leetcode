d07f208e728373e6c4648fcc8f9a52669fa09365
18plus <gosin.gu@gmail.com>
2016-03-07 16:59:39 +0800

    Update 3SumClosest.go

--- 3SumClosest.go
+++ 3SumClosest.go
@@ -1,3 +1,19 @@
+// Source : https://oj.leetcode.com/problems/3sum-closest/
+// Author : 18plusui
+// Date   : 2016-03-07
+
+/********************************************************************************** 
+* 
+* Given an array S of n integers, find three integers in S such that the sum is 
+* closest to a given number, target. Return the sum of the three integers. 
+* You may assume that each input would have exactly one solution.
+* 
+*     For example, given array S = {-1 2 1 -4}, and target = 1.
+* 
+*     The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+* 
+*               
+**********************************************************************************/
 package main
 
 import (
