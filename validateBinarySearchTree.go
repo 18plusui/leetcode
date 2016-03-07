9c7b62bab50f9dd8d0657ee4a45de8c06c08e95f
18plus <gosin.gu@gmail.com>
2016-03-07 17:07:15 +0800

    Update validateBinarySearchTree.go

--- validateBinarySearchTree.go
+++ validateBinarySearchTree.go
@@ -1,3 +1,38 @@
+// Source : https://oj.leetcode.com/problems/validate-binary-search-tree/
+// Author : 18plusui
+// Date   : 2016-03-07
+
+/********************************************************************************** 
+* 
+* Given a binary tree, determine if it is a valid binary search tree (BST).
+* 
+* Assume a BST is defined as follows:
+* 
+* The left subtree of a node contains only nodes with keys less than the node's key.
+* The right subtree of a node contains only nodes with keys greater than the node's key.
+* Both the left and right subtrees must also be binary search trees.
+* 
+* confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+* 
+* OJ's Binary Tree Serialization:
+* 
+* The serialization of a binary tree follows a level order traversal, where '#' signifies 
+* a path terminator where no node exists below.
+* 
+* Here's an example:
+* 
+*    1
+*   / \
+*  2   3
+*     /
+*    4
+*     \
+*      5
+* 
+* The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}". 
+* 
+*               
+**********************************************************************************/
 package main
 
 import (
