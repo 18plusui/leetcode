5e64ac93b55238d75587e0c22e2587a65b4f85fe
18plus <gosin.gu@gmail.com>
2016-03-07 17:02:13 +0800

    Update wordSearch.go

--- wordSearch.go
+++ wordSearch.go
@@ -1,3 +1,30 @@
+// Source : https://oj.leetcode.com/problems/word-search/
+// Author : 18plusui
+// Date   : 2016-03-07
+
+/********************************************************************************** 
+* 
+* Given a 2D board and a word, find if the word exists in the grid.
+* 
+* The word can be constructed from letters of sequentially adjacent cell, 
+* where "adjacent" cells are those horizontally or vertically neighboring. 
+* The same letter cell may not be used more than once.
+* 
+* For example,
+* Given board = 
+* 
+* [
+*   ["ABCE"],
+*   ["SFCS"],
+*   ["ADEE"]
+* ]
+* 
+* word = "ABCCED", -> returns true,
+* word = "SEE", -> returns true,
+* word = "ABCB", -> returns false.
+* 
+*               
+**********************************************************************************/
 package main
 
 import (
