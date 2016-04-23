0355835ce0ffeb335b63ca2b048403ebbfdb9791
18plus <gosin.gu@gmail.com>
2016-04-23 19:41:54 +0800

    Update mergeKSortedLists.go

--- mergeKSortedLists.go
+++ mergeKSortedLists.go
@@ -13,21 +13,6 @@ package main
 import (
 	"fmt"
 )
-// Source : https://oj.leetcode.com/problems/merge-k-sorted-lists/
-// Author : 18plusui
-// Date   : 2016-04-23
-
-/**********************************************************************************
-*
-* Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
-*
-***********************************************************************************/
-
-package main
-
-import (
-	"fmt"
-)
 
 func mergeKLists(lists []*ListNode) *ListNode {
 	dummy := new(ListNode)
