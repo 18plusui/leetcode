b06b79f02c4071087ecb3c2bf6f41ff75eba9e1e
18plus <gosin.gu@gmail.com>
2016-04-23 22:15:43 +0800

    Update mergeKSortedLists.go

--- mergeKSortedLists.go
+++ mergeKSortedLists.go
@@ -91,3 +91,82 @@ func main() {
 	ln := mergeKLists(lns)
 	printList(ln)
 }
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type Item struct {
+	value    int
+	priority int
+	index    int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) update(item *Item, value int, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
+
+func main() {
+	list1 := []int{1, 2, 3, 4, 6}
+	list2 := []int{1, 2, 3, 7, 6}
+	pq := make(PriorityQueue, len(list1))
+	i := 0
+	for priority, value := range list1 {
+		pq[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
+	}
+	heap.Init(&pq)
+
+	for priority, value := range list2 {
+
+		item := &Item{
+			value:    value,
+			priority: priority,
+		}
+		heap.Push(&pq, item)
+	}
+
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*Item)
+		fmt.Print(item.value)
+	}
+}
+
